de7206e79fe8085b602388ececb2067c4c996c46
choidoyeong <omok5674@naver.com>
2019-04-30 10:55:11 +0900

    hide secret key and make models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modefied = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 200)
+
+class Post(TimeStampedModel):
+    post_title = models.CharField(max_length = 200)
+    post_content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,)
+    like = models.IntegerField(defualt=0)
+
+class Opinion(TimeStampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASECADE)
+    opinion_content = models.TextField()
+    post_content = models.TextField()
+    user = models.Forignkey(User)
+
+class Comment(TimeStampedModel):
+    opinion = models.ForeignKey(Opinion, on_delete=models.CASECADE)
+    comment_content = models.TextField()
+    from_user = models.ForeignKey(User)
+    to_user = models.ForeignKey(User)
